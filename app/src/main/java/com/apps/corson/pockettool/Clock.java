c69d3a7b59115dc1c5ea95a47920ee5c8fffdcb4
Brian <brncherin@gmail.com>
2016-05-28 23:27:53 -0400

    first commit

--- app/src/main/java/com/apps/corson/pockettool/Clock.java
+++ app/src/main/java/com/apps/corson/pockettool/Clock.java
@@ -4,19 +4,31 @@ package com.apps.corson.pockettool;
  * Created by Brian on 5/28/16.
  */
 
+import android.widget.Toast;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Clock {
+
     public static String getTime(){
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
         return (sdf.format(cal.getTime()));
     }
 
-    public void vibrateTime(){
+    public static void timer(int length){  //length in miliseconds
 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("TIMETIMERITIMERIMTIERMTIMERIMETIEMTIMERTIMERTIMERTIMERTIMER");
+            }
+        },length);
     }
 
 }
